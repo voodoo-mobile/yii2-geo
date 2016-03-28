a202af7d7d61cefde76073509f41647d95104aa3
Dmitry Pisarev <d1mich@voodoo-mobile.com>
2016-03-28 12:54:11 +0600

    [!] Added showing lat and lng in vm-geo-location info

--- src/widgets/views/geo-location.php
+++ src/widgets/views/geo-location.php
@@ -5,10 +5,16 @@ use \vm\geo\components\GeoLocationAsset;
 
 /** @var bool $isLocated */
 /** @var bool $toUpdate */
+/** @var float $lat */
+/** @var float $lng */
 
 GeoLocationAsset::register($this);
 ?>
 
-<div class="vm-geo-location" data-is-located="<?= intval($isLocated) ?>" data-to-update="<?= intval($toUpdate) ?>"
-     data-url="<?= Url::to
-(['/geo/coordinates/update']) ?>"></div>
+<div class="vm-geo-location"
+     data-is-located="<?= intval($isLocated) ?>"
+     data-to-update="<?= intval($toUpdate) ?>"
+     data-lat="<?= $lat ?>"
+     data-lng="<?= $lng ?>"
+     data-url="<?= Url::to(['/geo/coordinates/update']) ?>">
+</div>
